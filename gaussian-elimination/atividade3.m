543620c017a4df645d97e4c320eb72fab7162602
luiz-henrique-souza-santos <luizsouzaa080@gmail.com>
2025-11-12 23:46:08 -0300

    feat: Gaussian elimination function (forward elimination and backward replacement)

--- gaussian-elimination/atividade3.m
+++ gaussian-elimination/atividade3.m
@@ -7,38 +7,51 @@ function atividade3 ()
           12;
          -11];
 
-    Aa = [A B];
+    num_lines = size(A,1);
 
-    Aa = pivot(A)
+    printf('Augmented Matrix A|B\n');
+    AB = [A B]
 
-    Aa = gaussian_elimination(Aa)
+    printf('\nA|B matrix after partial pivoting\n');
+    AB_pivot = pivot(AB, num_lines)
+
+    printf('\nSolution after Gaussian elimination\n');
+    solution = gaussianElimination(AB, num_lines)
 end
 
-function Aa = pivot(Aa)
-    %Loop to the last column of the original matrix A.
-    for i = 1 : size(Aa,1) - 1
+function AB = pivot(AB, num_lines)
+    %Loop to the next to last column of the original matrix A.
+    for i = 1 : num_lines - 1
         %Returns the index of max absolute value in the range (below and the main diagonal) of column 'i'.
-        [~, idx_max_value] = max(abs(Aa(i:end, i)));
+        [~, idx_max_value] = max(abs(AB(i:end, i)));
 
         %Get the line of pivot element (the element on the main diagonal).
-        temp = Aa(i, :);
+        temp = AB(i, :);
 
         %Pivot between the line of the max absolute value and the line of the element on main diagonal.
-        Aa(i, :) = Aa(idx_max_value + i - 1, :);
-        Aa(idx_max_value + i - 1, :) = temp;
+        AB(i, :) = AB(idx_max_value + i - 1, :);
+        AB(idx_max_value + i - 1, :) = temp;
     end
 end
 
-function Aa = gaussian_elimination(Aa)
-    solution = zeros (size(Aa,1) , 1);
-
-    for i = 1 : size(Aa,1) - 1
-        for j = 1 : size(Aa,1) - i
-        factor= Aa(i+1,i) / Aa(i,i);
-        Aa(i+j, :) = Aa(i+j, :) - factor * Aa(i, :);
+function solution = gaussianElimination(AB, num_lines)
+    %Loop over the columns, going from the first to the penultimate
+    for i = 1 : num_lines - 1
+        %Loop over the rows below the current pivot element 'i'. Eliminating elements below it.
+        for j = i + 1 : num_lines
+        factor= AB(j,i) / AB(i,i);
+        AB(j, :) = AB(j, :) - factor * AB(i, :);
         end
     end
+
+    solution = zeros(num_lines , 1);
+    %Calculates the last element of the solution vector.
+    solution(num_lines) = AB(num_lines, end) / AB(num_lines, num_lines);
+
+    %Backward substitution loop.
+    for i = num_lines - 1 : -1 : 1
+      %Calculates terms already solved (to the right of the current pivot)
+      calculated_term = AB(i, i+1 : num_lines) * solution(i+1: num_lines);
+      solution(i) = ( AB(i, end ) - calculated_term) / AB(i,i);
+    end
 end
-
-
-
